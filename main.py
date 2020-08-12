2577606b25d6290aa519be304af065b4aaabd255
Nitish Sahni <sahninitish@gmail.com>
2020-08-12 16:43:36 +0530

    Done

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from NSDL.nsdl import Read_NSDL
 from Classes.classes import Person
 import time
+import dill
+import os
+import PyPDF2
+import pikepdf as pikepdf
+import pandas as pd
 
 
-if __name__ == '__main__':
+def run():
     start_time = time.time()
-    # Your name and PAN Card Number go below
-    p = Person('Full Name', 'PANCARDNO')
+    with open('NSDL/models/nsdl.pkl', 'rb') as s:
+        nsdl_reader = dill.load(s)
     try:
-        Read_NSDL(person=p)
+        p = Person('FULL NAME', 'PAN_NUMBER')
+        # Example -> p = Person('Nitish Sahni', 'GBXPS0000T')
+        nsdl_reader(p)
     except Exception as e:
         print(e)
     print("Program took", time.time() - start_time, "to run")
+
+
+if __name__ == '__main__':
+    run()
